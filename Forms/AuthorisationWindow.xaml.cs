d1bb2f17d8072528298e5245021037573950fa95
greendend <alexakasmern@tut.by>
2020-07-24 23:50:07 +0300

    feat: added texture layer of war local map

--- Forms/AuthorisationWindow.xaml.cs
+++ Forms/AuthorisationWindow.xaml.cs
@@ -60,9 +60,9 @@ namespace LandConquest
         private void AuthorisationWindow_Loaded(object sender, RoutedEventArgs e)
         {
             //greendend
-            //connection = new SqlConnection(@"Data Source=DESKTOP-3S39QPO\SQLEXPRESS;Initial Catalog=LandConquestDB;Integrated Security=True;Pooling=False");
+            connection = new SqlConnection(@"Data Source=DESKTOP-3S39QPO\SQLEXPRESS;Initial Catalog=LandConquestDB;Integrated Security=True;Pooling=False");
             //user-pass 
-            connection = new SqlConnection(@"Data Source=DESKTOP-EQUN2R7;Initial Catalog=LandConquestDB;Integrated Security=True;Pooling=False");
+            //connection = new SqlConnection(@"Data Source=DESKTOP-EQUN2R7;Initial Catalog=LandConquestDB;Integrated Security=True;Pooling=False");
             //glandeil
             //connection = new SqlConnection(@"Data Source=LEXICH\SQLEXPRESS;Initial Catalog=LandConquestDB;Integrated Security=True;Pooling=False");
             //online connection link
--- Forms/WarWindow.xaml.cs
+++ Forms/WarWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using NTiled;
 
 namespace LandConquest.Forms
 {
@@ -22,11 +19,57 @@ namespace LandConquest.Forms
     /// </summary>
     public partial class WarWindow : Window
     {
+        System.Windows.Controls.Primitives.UniformGrid localWarMap = new System.Windows.Controls.Primitives.UniformGrid();
+        
+        //Canvas localWarArmyLayer = new Canvas();
         public WarWindow()
         {
-            TiledReader tiledReader = new TiledReader();
-                var map = tiledReader.Read(@"map.tmx");
-            warDataGrid.ItemsSource = map.Tilesets;
+            InitializeComponent();
+            int columnWidth = 20;
+            int rowHeight = 20;
+            localWarMap.Columns = 50;
+            localWarMap.Rows = 40;
+            localWarMap.Width = columnWidth * localWarMap.Columns;
+            localWarMap.Height = rowHeight * localWarMap.Rows;
+            localWarMap.HorizontalAlignment = HorizontalAlignment.Left;
+            localWarMap.VerticalAlignment = VerticalAlignment.Center;
+            Loaded += WarWin_Loaded;
         }
+
+        private void WarWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Rectangle myRectangle1 = new Rectangle();
+            //Grid localWarArmyLayer = new Grid();
+            //mainWarWinGrid.SetZIndex(myRectangle1, 3);
+            System.Windows.Controls.Image imgArmy = new System.Windows.Controls.Image();
+            //imgArmy.SetZIndex();
+            imgArmy.Source = new BitmapImage(new Uri("/Pictures/warrior.png", UriKind.Relative));
+            imgArmy.Height = 20;
+            imgArmy.Width = 20;
+            imgArmy.Margin = new Thickness(400, 400, 0, 0);
+
+            mainWarWinGrid.Children.Add(imgArmy);
+
+            for (int x = 0; x < localWarMap.Columns; x++)
+            {
+                for (int y = 0; y < localWarMap.Rows; y++)
+                {
+                    System.Windows.Controls.Image img = new System.Windows.Controls.Image();
+                    img.Source = new BitmapImage(new Uri("/Pictures/test-tile.jpg", UriKind.Relative));
+                    localWarMap.Children.Add(img);
+                }
+            }
+            mainWarWinGrid.Children.Add(localWarMap);
+            //test 
+            
+        }
+
+        //private void mainWarWinGrid_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    Grid mainWarWinGrid = (Grid)sender;
+
+        //    mainWarWinGrid.Children.Add(localWarMap);
+        //    Console.WriteLine("govna pojui!");
+        //}
     }
 }
