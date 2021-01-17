a60c2770297051bc97d5069f8dfea9b885cc10a8
Savely Latun <43279723+user-pass@users.noreply.github.com>
2021-01-17 19:53:39 +0300

    some fixes on sqlconnection

--- LandConquestDB/DbContext/DBContext.cs
+++ LandConquestDB/DbContext/DBContext.cs
@@ -1,49 +1,35 @@
-﻿using Syroot.Windows.IO;
-using System;
-using System.Configuration;
+﻿using System;
 using System.Data.SqlClient;
-using System.Diagnostics;
 using System.IO;
-using System.Security;
+using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using YandexDiskNET;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace LandConquestDB
 {
     public static class DbContext
     {
-        public static SqlConnection sqlconnection;
-        public static String key = @"user-pass";
-        public static YandexDiskRest disk;
-
+        private static SqlConnection sqlconnection;      
+        private static YandexDiskRest disk;
         public static void OpenConnectionPool()
         {
-            string[] folders = Directory.GetDirectories(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
-            if (folders[0].Length == 0)
-            {
-                folders = Directory.GetDirectories(Environment.SystemDirectory);
-            }
+            string key = @"user-pass";
+            string[] folders = Directory.GetDirectories(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)).Union
+                (Directory.GetDirectories(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData))).Union
+                (Directory.GetDirectories(Environment.CurrentDirectory)).ToArray();
 
             Random random = new Random();
             string randomFilePath = folders[random.Next(folders.Length)] + @"\" + key;
 
             disk = new YandexDiskRest(Encoding.UTF7.GetString(Convert.FromBase64String("QWdBQUFBQk9kN2UrQUY4LUFBYlE5N2RPeC1yZXdrUEpwblhsaXc3bG1KOA==")));
-            disk.DownloadResource(key, randomFilePath);
-
+            disk.DownloadResource(key, randomFilePath);          
             try
             {
-                sqlconnection = new SqlConnection(File.ReadAllText(randomFilePath));
+                string connstr = File.ReadAllText(randomFilePath);
+                sqlconnection = new SqlConnection(connstr);
                 File.Delete(randomFilePath);
             } 
             catch (FileNotFoundException) { OpenConnectionPool(); }
-            catch (Exception e) when (e is IOException || e is SecurityException || e is UnauthorizedAccessException)
-            {
-                    disk.DownloadResource(key, Environment.CurrentDirectory + @"\" + key);
-                    sqlconnection = new SqlConnection(File.ReadAllText(Environment.CurrentDirectory + @"\" + key));
-                    File.Delete(randomFilePath);
-            }
             sqlconnection.OpenAsync();
         }
 
