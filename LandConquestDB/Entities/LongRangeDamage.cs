da6959f34157ce8f5a6ff8a940d5d12495caca8f
Smirnoff <alexakasmern@tut.by>
2021-01-18 23:21:46 +0300

    feat: start working with long range fighting

---
+++ LandConquestDB/Entities/LongRangeDamage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandConquestDB.Entities
+{
+    class LongRangeDamage
+    {
+        [Required]
+        [Column("army_id")]
+        [StringLength(16)]
+        public string ArmyId { get; set; }
+
+
+        [Required]
+        [Column("damage")]
+        public int Damage { get; set; }
+    }
+}
