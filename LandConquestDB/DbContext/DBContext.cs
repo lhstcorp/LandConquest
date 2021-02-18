69d0f95404ccead615048afcea87dda4720522a2
Savely Latun <43279723+user-pass@users.noreply.github.com>
2021-02-18 20:04:09 +0300

    feat: fixed error

--- LandConquestDB/DbContext/DBContext.cs
+++ LandConquestDB/DbContext/DBContext.cs
@@ -9,7 +9,7 @@ namespace LandConquestDB
         private static SqlConnection sqlconnection;
         public static void OpenConnectionPool()
         {
-            var reference = "user-pass";
+            var reference = "online_old.txt";
             try
             {
                 sqlconnection = new SqlConnection(YDContext.ReadResource(reference));
