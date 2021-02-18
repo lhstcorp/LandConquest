ffebf07dc577e8306973d65b4d049367b9c8bd82
Savely Latun <43279723+user-pass@users.noreply.github.com>
2021-02-18 15:18:35 +0300

    feat: message window dev

---
+++ DialogWIndows/EnterTextDialogWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+namespace LandConquest.DialogWIndows
+{
+    public partial class EnterTextDialogWindow : Window
+    {
+        public EnterTextDialogWindow(string _header)
+        {
+            InitializeComponent();
+            LabelMessageHeader.Content = _header;
+        }
+
+        public EnterTextDialogWindow(string _header, string _button)
+        {
+            InitializeComponent();
+            LabelMessageHeader.Content = _header;
+            ButtonEnter.Content = _button;
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxMessageValue.Text != null)
+            {
+                this.DialogResult = true;
+            }
+        }
+
+        public string ValueText
+        {
+            get { return TextBoxMessageValue.Text; }
+        }
+    }
+}
