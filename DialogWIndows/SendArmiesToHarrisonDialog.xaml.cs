43ce5e2a8be238f855fa23463824f8b58f2e4d1e
Smirnoff <alexakasmern@tut.by>
2021-02-18 21:26:52 +0300

    feat: add some changes in harrison dialog;

---
+++ DialogWIndows/SendArmiesToHarrisonDialog.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LandConquest.DialogWIndows
+{
+    /// <summary>
+    /// Логика взаимодействия для SendArmiesToHarrisonDialog.xaml
+    /// </summary>
+    public partial class SendArmiesToHarrisonDialog : Window
+    {
+        public SendArmiesToHarrisonDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void sliderInfantry_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sliderInfantry.Value = 0;
+        }
+
+        private void sliderArchers_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sliderInfantry.Value = 0;
+        }
+
+        private void sliderKnights_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sliderInfantry.Value = 0;
+        }
+
+        private void sliderSiege_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            sliderInfantry.Value = 0;
+        }
+
+        private void CheckTypeAndReturnCount(object sender, MouseButtonEventArgs e)
+        {
+        }
+    }
+}
