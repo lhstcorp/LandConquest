48ea80e5184d2cc10aae8dfc9af06b61dc5332fd
Smirnoff <alexakasmern@tut.by>
2021-02-26 14:43:35 +0300

    feat: work on Land window;

---
+++ LandConquestDB/Entities/Castle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandConquestDB.Entities
+{
+    public sealed class Castle
+    {
+        public int LandId { get; set; }
+        public int CastleLvl { get; set; }
+        public int SlotsCount { get; set; }
+    }
+}
