8c32efb8ce79f3537b78401dd7de0057978135f4
Smirnoff <alexakasmern@tut.by>
2021-03-27 11:19:52 +0300

    feat: start working on data grid list for garrison view;

---
+++ LandConquestDB/Entities/GarrisonListings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandConquestDB.Entities
+{
+    public class GarrisonListings
+    {
+        public string ArmyId { get; set; }
+        public Image ArmyTypeImg { get; set; }
+        public int Inf { get; set; }
+
+        public int Ar { get; set; }
+        public int Kn { get; set; }
+        public int Sie { get; set; }
+        public int Total { get; set; }
+    }
+}
