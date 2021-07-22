b50dce1315b8cc1deee78f5d5a46c7a337ada604
Savely Latun <43279723+user-pass@users.noreply.github.com>
2021-07-22 03:05:21 +0300

    feat: chat update

--- LandConquestDB/Entities/ChatMessages.cs
+++ LandConquestDB/Entities/ChatMessages.cs
@@ -3,9 +3,12 @@
     using System;
     public class ChatMessages
     {
+        public string PlayerId { get; set; }
         public string PlayerName { get; set; }
         public string PlayerMessage { get; set; }
-        public DateTime MessageTime { get; set; }
+        public string PlayerTargetId { get; set; }
+        public string PlayerTargetName { get; set; }
+        public DateTime MessageSentTime { get; set; }
 
     }
 }
