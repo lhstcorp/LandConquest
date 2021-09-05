297acc18a2d99f3852239a0de322d357c2f87b7d
Savely Latun <43279723+user-pass@users.noreply.github.com>
2021-09-06 00:46:28 +0300

    feat: chat developing

---
+++ DialogWIndows/CreateNewPersonalDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using LandConquestDB.Models;
+using System.Windows;
+
+
+namespace LandConquest.DialogWIndows
+{
+    public partial class CreateNewPersonalDialog : Window
+    {
+        private string playerReceiverId;
+        public CreateNewPersonalDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxMessageValue.Text != null && TextBoxPlayerName.Text != null)
+            {
+                playerReceiverId = PlayerModel.ValidatePlayerByName(TextBoxPlayerName.Text);
+                if (playerReceiverId != null)
+                {
+                    this.DialogResult = true;
+                }
+            }
+        }
+
+        public string ValueText
+        {
+            get { return playerReceiverId + " " + TextBoxMessageValue.Text; }
+        }
+    }
+}
