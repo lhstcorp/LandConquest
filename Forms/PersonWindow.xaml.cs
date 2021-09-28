31a9cc402db90920ca7542cbe45955c5d7711741
Elizaveta1412 <79195511+Elizaveta1412@users.noreply.github.com>
2021-09-28 20:31:29 +0300

    feat: working on personalization direction;

---
+++ Forms/PersonWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using LandConquestDB.Entities;
+using LandConquestDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandConquest.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для PersonWindow.xaml
+    /// </summary>
+    public partial class PersonWindow : Window
+    {
+        Player player;
+
+        public PersonWindow(Player _player)
+        {
+            player = _player; 
+            InitializeComponent();
+            InitPersonCharacteristics();
+        }
+       
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public void InitPersonCharacteristics()
+        {
+            Person person = new Person();
+            person = PersonModel.GetPersonInfo(player, person);
+            Power.Content = person.Power;
+            Agility.Content = person.Agility;
+            Intelligence.Content = person.Intellect;
+            Level.Content = person.Level;
+            Health.Content = person.Health;
+            Experience.Content = person.Exp;
+            NameSurname.Content = person.Name +" "+ person.Surname;
+        }
+    }
+}
--- LandConquestDB/Models/PersonModel.cs
+++ LandConquestDB/Models/PersonModel.cs
@@ -12,8 +12,10 @@ namespace LandConquestDB.Models
     {
         public static void CreatePerson(Person _person)
         {
-            string query = "INSERT INTO dbo.PersonData (player_id, person_id, name, surname, maleFemale) " +
-                           "VALUES (@player_id, @person_id, @name, @surname, @maleFemale)";
+            string query = "INSERT INTO dbo.PersonData (player_id, person_id, name, surname, maleFemale, power, agility," +
+                                                       "health, intellect) " +
+                           "VALUES (@player_id, @person_id, @name, @surname, @maleFemale, @power, @agility, @health," +
+                                   "@intellect)";
             var command = new SqlCommand(query, DbContext.GetSqlConnection());
 
             command.Parameters.AddWithValue("@player_id", _person.PlayerId);
@@ -21,16 +23,55 @@ namespace LandConquestDB.Models
             command.Parameters.AddWithValue("@name", _person.Name);
             command.Parameters.AddWithValue("@surname", _person.Surname);
             command.Parameters.AddWithValue("@maleFemale", _person.MaleFemale);
-            command.Parameters.AddWithValue("@prestige", _person.Prestige);
-            command.Parameters.AddWithValue("@level", _person.Level);
-            command.Parameters.AddWithValue("@exp", _person.Exp);
-            command.Parameters.AddWithValue("@exp", _person.Power);
-            command.Parameters.AddWithValue("@exp", _person.Agility);
-            command.Parameters.AddWithValue("@exp", _person.Health);
-            command.Parameters.AddWithValue("@exp", _person.Intellect);
+            //command.Parameters.AddWithValue("@prestige", _person.Prestige);
+            //command.Parameters.AddWithValue("@lvl", _person.Level);
+            //command.Parameters.AddWithValue("@exp", _person.Exp);
+            command.Parameters.AddWithValue("@power", _person.Power);
+            command.Parameters.AddWithValue("@agility", _person.Agility);
+            command.Parameters.AddWithValue("@health", _person.Health);
+            command.Parameters.AddWithValue("@intellect", _person.Intellect);
+
+
 
             command.ExecuteNonQuery();
             command.Dispose();
+        }
+        public static Person GetPersonInfo(Player player, Person person)
+        {
+            string query = "SELECT * FROM dbo.PersonData WHERE person_id = @person_id";
+
+            var command = new SqlCommand(query, DbContext.GetSqlConnection());
+            command.Parameters.AddWithValue("@person_id", player.PlayerId);
+
+            using (var reader = command.ExecuteReader())
+            {
+                var playerId = reader.GetOrdinal("player_id");
+                var personId = reader.GetOrdinal("person_id");
+                var name = reader.GetOrdinal("name");
+                var surname = reader.GetOrdinal("surname");
+                var maleFemale = reader.GetOrdinal("maleFemale");
+                var power = reader.GetOrdinal("power");
+                var agility = reader.GetOrdinal("agility");
+                var health = reader.GetOrdinal("health");
+                var intellect = reader.GetOrdinal("intellect");
+
+                while (reader.Read())
+                {
+                    player.PlayerId = reader.GetString(playerId);
+                    person.PersonId = reader.GetString(personId);
+                    person.Name = reader.GetString(name);
+                    //person.Surname = reader.GetString(surname);
+                    person.MaleFemale = reader.GetInt32(maleFemale)>0;
+                    person.Power = reader.GetInt32(power);
+                    person.Agility = reader.GetInt32(agility);
+                    person.Health = reader.GetInt32(health);
+                    person.Intellect = reader.GetInt32(intellect);
+                }
+                reader.Close();
+            }
+            command.Dispose();
+
+            return person;
         }
     }
 }
