84742bbb656c542968abfcd0b8c6416576c20183
Savely Latun <43279723+user-pass@users.noreply.github.com>
2021-10-27 04:55:35 +0300

    feat: added report dialog

---
+++ DialogWIndows/WelcomeBackReportDialogWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LandConquest.DialogWIndows
+{
+    public partial class WelcomeBackReportDialogWindow : Window
+    {
+        public WelcomeBackReportDialogWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
