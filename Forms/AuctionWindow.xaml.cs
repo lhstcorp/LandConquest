edd5d218fef60b3046fd6efb3371907042e26b36
Savely Latun <43279723+user-pass@users.noreply.github.com>
2021-11-22 06:21:05 +0300

    feat: styles, mvvm

--- Forms/AuctionWindow.xaml.cs
+++ Forms/AuctionWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using LandConquestDB.Entities;
+﻿using LandConquest.WindowViewModels;
+using LandConquestDB.Entities;
 using System.Windows;
 
 namespace LandConquest.Forms
@@ -9,52 +10,6 @@ namespace LandConquest.Forms
         {
             InitializeComponent();
             DataContext = new AuctionWindowViewModel(_player);
-        }
-
-        private void buttonClose_Click(object sender, RoutedEventArgs e)
-        {
-            ((AuctionWindowViewModel)DataContext).CloseWindow();
-        }
-
-        private void buttonCreateListing_Click(object sender, RoutedEventArgs e)
-        {
-            ((AuctionWindowViewModel)DataContext).CreateListing();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            ((AuctionWindowViewModel)DataContext).WindowLoaded();
-        }
-
-        private void buttonFindListing_Click(object sender, RoutedEventArgs e)
-        {
-            ((AuctionWindowViewModel)DataContext).FindListing();
-        }
-
-        private void buttonShowMyListings_Click(object sender, RoutedEventArgs e)
-        {
-            ((AuctionWindowViewModel)DataContext).ShowMyListings();
-        }
-
-        private void buttonBuy_Click(object sender, RoutedEventArgs e)
-        {
-            ((AuctionWindowViewModel)DataContext).Buy();
-
-        }
-
-        private void buttonDelete_Click(object sender, RoutedEventArgs e)
-        {
-            ((AuctionWindowViewModel)DataContext).ButtonDeleteClick();
-        }
-
-        private void auctionDataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            ((AuctionWindowViewModel)DataContext).DataGridSelectionChanged();
-        }
-
-        private void buttonUpdateListings_Click(object sender, RoutedEventArgs e)
-        {
-            ((AuctionWindowViewModel)DataContext).UpdateListings();
         }
     }
 }
